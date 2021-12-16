4fabbc596184c503bfa349aec787885f61abaeb4
virginia zapata <virjanzala@gmail.com>
2021-12-16 15:31:30 -0300

    modificaçaõ de numero  PORTO

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express'); //importacao do pacote
 const app = express(); //instanciando express
 const {connection} = require('./app/database/db');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 
 // Middleware   
