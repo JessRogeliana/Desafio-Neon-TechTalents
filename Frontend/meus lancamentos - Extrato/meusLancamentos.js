a6ec8e45f52ba1f4d194ceaf8edeb7fd49bf74b2
JessRogeliana <kuroi.taiga@gmail.com>
2021-12-18 22:56:43 -0300

    Alterações javascript: botão excluir funcionando, formulario pegando info tabela ok, mexi na função filtro, e ainda pendente edição tabela

--- Frontend/meus lancamentos - Extrato/meusLancamentos.js
+++ Frontend/meus lancamentos - Extrato/meusLancamentos.js
@@ -1,130 +1,100 @@
-/* CRIAR LANÇAMENTOS TABELA */
+const listaExtrato = [{        
+    id: 1,
+    item: 'Água',
+    data: '2021-10-11',
+    tipo: 'entrou',
+    valor: 20.00
+}, {        
+    id: 2,
+    item: 'luz',
+    data: '2021-05-09',
+    tipo: 'saiu',
+    valor: 100.00
+} ]  
 
-var tabela = document.getElementById('tabela-lancamento')
+window.addEventListener('DOMContentLoaded', () =>  {
 
-function extraiInfoApi() {
-    var lancamento = {
-        id: id,
-        item: item,
-        data: data,
-        tipo: tipo,
-        valor: valor
+    /* chamada api */
+
+    let trList = '';
+    for (let i = 0; i < listaExtrato.length; i++) {
+        var linhaExtrato = listaExtrato[i]
+        trList += 
+        `
+        <tr id="lancamento" class="lancamento">
+
+            <td class="hide" class="id">${linhaExtrato.id}</td>
+            <td class="item" scope="row">${linhaExtrato.item}</td>
+            <td class="data">${linhaExtrato.data}</td>
+            <td class="tipo">${linhaExtrato.tipo}</td>
+            <td class="valor false">R$ ${linhaExtrato.valor}</td>
+            <td class="acoes">
+                <button type="button" class="btn editingTRbutton" data-bs-toggle="modal" data-target="#editModal" data-bs-target="#exampleModal" onclick="abrirModal(${i})"> 
+                    <svg class="icon-editar" xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>                                
+                    </svg>
+                </button>
+                <svg class="icon-excluir" value="Delete" onclick="deleteRow(this)" xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+            </td>
+        </tr>
+    `
     }
-    return lancamento
+    document.getElementById('tabelaLancamento').innerHTML = trList;
+    
+})
+
+function abrirModal(index) {
+    alteraCamposModal(listaExtrato[index]);
 }
 
-function montaTd(dado, classe) {
-    var td = document.createElement("td");
-    td.textContent = dado;
-    td.classList.add(classe);
-    return td
+/* Pega valor da tabela e insere no form/modal */
+function alteraCamposModal(linhaExtrato) {
+    document.getElementById('item').value = linhaExtrato.item
+    document.getElementById('data').value = linhaExtrato.data
+    document.getElementById('tipo').value = linhaExtrato.tipo
+    document.getElementById('valor').value = linhaExtrato.valor
 }
 
-function montaTr(lancamento) {
-    var lancamentoTR = document.createElement("tr");
-    pacienteTr.classList.add('lancamento');
+/*event -> salvar -> trocar dados para tabela -> usar elementos por id */
 
-    var idTd = montaTd('lancamento.id', "hide");
-    idTd.classList.add('id');
-    var itemTd = montaTd('lancamento.item', "item");
-    var dataTd = montaTd('lancamento.data', "data");
-    var tipoTd = montaTd('lancamento.tipo', "tipo");
-    var valorTd = montaTd('lancamento.valor', "valor");
-    if (tipoTd.value == "saiu") {
-        valorTd.classList.add('false')
-    } else {
-        valorTd.classList.add('true')
-    }
-    var acoesTd = document.createElement('td');
+/* Pega valor inserido/atual do form/modal */
+function submeterFormulario() {
+    const item = document.getElementById('item').value 
+    const data = document.getElementById('data').value
+    const tipo = document.getElementById('tipo').value
+    const valor = document.getElementById('valor').value
 
-    lancamentoTR.appendChild(idTd);
-    lancamentoTR.appendChild(itemTd);
-    lancamentoTR.appendChild(dataTd);
-    lancamentoTR.appendChild(tipoTd);
-    lancamentoTR.appendChild(valorTd);
-    lancamentoTR.appendChild(acoesTd);
-
-    return lancamentoTR
-}
-
-function adicionaTabela(id, item, data, tipo, valor) {
-    var lancamento = extraiInfoApi();
-    var lancamentoTR = montaTr(lancamento);
-
-    tabela.appendChild(lancamentoTR);
+    listaExtrato[index][1] = item.value;
+    listaExtrato[index][2] = data.value;
+    listaExtrato[index][3] = tipo.value;
+    listaExtrato[index][4] = valor.value; 
 }
 
 
-
-/* CRIAR TD AÇÕES */
-function preencheTdAcoes() {
-    var botaoTdAcoes = document.createElement('button');
-    var svgTdAcoesEditar = document.createElement('svg');
-    var svgTdAcoesExcluir = document.createElement('svg');
-    var pathTdAcoesEditar = document.createElement('path');
-    var pathTdAcoesExcluirA = document.createElement('path');
-    var pathTdAcoesExcluirB = document.createElement('path');
-
-    pathTdAcoesEditar.textContent = 'd="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"';
-
-    pathTdAcoesExcluirA.textContent = 'd="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"'
-
-    pathTdAcoesExcluirB.textContent = 'fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"'
-
-    svgTdAcoesEditar.appendChild('pathTdAcoesEditar');
-    svgTdAcoesExcluir.appendChild('pathTdAcoesExcluirA');
-    svgTdAcoesExcluir.appendChild('pathTdAcoesExcluirB');
-
-    botaoTdAcoes.appendChild('svgAcoesEditar');
-
-    acoesTd.appendChild('botaoTdAcoes');
-    acoesTd.appendChild('svgTdAcoesExcluir');
-}
-
-
-
-
-/* EDITAR E EXCLUIR DADOS TABELA */
-var lancamentos, index;
-var botaoAcoes = document.querySelector('btn')
-
-function alteraTabela(item, data, tipo, valor) {
-    lancamentos.rows[index].cells[1].innerHTML = item;
-    lancamentos.rows[index].cells[2].innerHTML = data;
-    lancamentos.rows[index].cells[3].innerHTML = tipo;
-    lancamentos.rows[index].cells[4].innerHTML = valor;
-}
-
-function preencheCamposForm() {
-    for(var i=0; i < lancamentos.rows.length; i ++) {
-
-        lancamentos.rows[i].botaoAcoes.onclick = function() {
-            index = this.rowIndex;
-            document.getElementById("description").value = lancamentos.rows[index].cells[1].innerText;
-            document.getElementById("date").value = lancamentos.rows[index].cells[2].innerText;
-            document.getElementById("status").value = lancamentos.rows[index].cells[3].innerText;
-            document.getElementById("amount").value = lancamentos.rows[index].cells[4].innerText;
-        }
-    }
-}
+/* Substituir valor tabela pelo inserido no form */
 /*
-function excluiLancamento() {
-    for(var i = 0; i< pessoas.rows.length; i++) {
-        if ( index == i) {
-            lancamentos.deleteRow(index);
-            return 
-        }
-    }
+function alteraCamposModal(linhaExtrato) {
+    document.getElementById('submeterFormulario').addEventListener('click', function () {
+
+    })
 }
+alteraCamposModal(listaExtrato[index]);
+
 */
 
-var listaLancamentos = document.querySelectorAll('lancamento');
-listaLancamentos.forEach(function(lancamento) {
-    lancamento.addEventListener('click',function() {
-        console.log('fui clicado');
-        this.remove();
-        //target.parentNode.remove()
-    })
+
+
+
+/*  EXCLUIR DADOS TABELA */
+$(document).on("click", ".icon-excluir", function () {
+    var botaoExcluir = $(this).index('.icon-excluir') + 1;
+    console.log("after deletion");
+    console.log(botaoExcluir);
+      
+    document.getElementById("tabela").deleteRow(botaoExcluir);      
 });
 
 
@@ -179,48 +149,6 @@ function somaCamposValor() {
 
 
 /* CHAMADAS */
-
-
-
-
-
-
-
-
-
-
-/* ----------  OUTRA OPÇÃO --------------- */
-
-/* CRIAR LANÇAMENTOS TABELA (a partir de info cadastrada na página de Honer - Fazer chamada pela API) */
-/*
-function insereTabela(id, item, data, tipo, valor, acoes) {
-
-    var lancamentos = document.getElementById("tabela");
-    var qtdLinhas = lancamentos.rows.length;
-    var linha = lancamentos.insert(qtdLinhas);
-
-    var cellId = linha.insertCell(0);
-    var cellItem = linha.insertCell(1);
-    var cellData = linha.insertCell(2);
-    var cellTipo = linha.insertCell(3);
-    var cellValor = linha.insertCell(4);
-    var cellAcoes = linha.insertCell(5);
-
-    cellId.innerHTML = id;
-    cellItem.innerHTML = item;
-    cellData.innerHTML = data;
-    cellTipo.innerHTML = tipo;
-    cellValor.innerHTML = valor;
-    cellAcoes.innerHTML = acoes;
-
-    preencheCamposForm()
-}
-
-document.querySelector("botao-novo-lancamento").addEventListener('onClick', insereTabela(parametros chamar pela API));
-*/
-
-
-/* CHAMADA DA API (?) */ 
 
 
 
